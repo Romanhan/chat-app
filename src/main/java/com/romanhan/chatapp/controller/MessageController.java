fc2c8f6d8fe4b518f9832ceae0e741bce73fb05e
Romanhan <hanmamedov.r@gmail.com>
2025-11-06 13:55:25 +0200

    feat: add real-time typing indicators with animated dots

--- src/main/java/com/romanhan/chatapp/controller/MessageController.java
+++ src/main/java/com/romanhan/chatapp/controller/MessageController.java
@@ -2,6 +2,7 @@ package com.romanhan.chatapp.controller;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.romanhan.chatapp.model.Message;
@@ -21,5 +22,11 @@ public class MessageController {
     public Message sendMessage(Message message) {
         messageService.saveMessage(message);
         return message;
+    }
+
+    @MessageMapping("/typing")
+    @SendTo("/topic/typing")
+    public String handleTyping(String username) {
+        return username;
     }
 }
